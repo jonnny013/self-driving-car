bfe8e5824bc33dff6eab7c56aea4a3a99353eed7
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-19 14:34:17 +0800

    update road component

--- road.js
+++ road.js
@@ -1,10 +1,10 @@
 class Road {
-  constructor(x, width, laneCount = 3) {
+  constructor(x, width, laneCount = 4) {
     this.x = x
     this.width = width
     this.laneCount = laneCount
 
-    this.left = x - width/2
+    this.left = x - width / 2
     this.right = x + width / 2
 
     const infinity = 100000000
@@ -12,18 +12,28 @@ class Road {
     this.bottom = infinity
   }
 
+  getLaneCenter(laneIndex) {
+    const laneWidth = this.width / this.laneCount
+    return this.left + laneWidth / 2 + laneIndex * laneWidth
+  }
+
   draw(ctx) {
     ctx.lineWidth = 5
     ctx.strokeStyle = 'white'
 
-    ctx.beginPath()
-    ctx.moveTo(this.right, this.top)
-    ctx.lineTo(this.right, this.bottom)
-    ctx.stroke()
-
-    ctx.beginPath()
-    ctx.moveTo(this.left, this.top)
-    ctx.lineTo(this.left, this.bottom)
-    ctx.stroke()
+    for (let i = 0; i < this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount)
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20])
+      } else {
+        ctx.setLineDash([])
+      }
+      ctx.beginPath()
+      ctx.moveTo(x, this.top)
+      ctx.lineTo(x, this.bottom)
+      ctx.stroke()
+    }
   }
 }
+
+
