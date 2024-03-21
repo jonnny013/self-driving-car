af3184befc59b25f0ff7a95cfcbb4d855d397249
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-21 11:51:56 +0800

    remove sensors from traffic and add speed variable

--- main.js
+++ main.js
@@ -4,8 +4,18 @@ canvas.width = 200
 
 const ctx = canvas.getContext('2d')
 const road = new Road(canvas.width / 2, canvas.width * 0.9)
-const car = new Car(road.getLaneCenter(1), 100, 30, 50)
- const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50)]
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS", 5)
+ const traffic = [
+   new Car(
+     road.getLaneCenter(1),
+     -100,
+     30,
+     50,
+     'DUMMY',
+     Math.floor(Math.random() * 5) + 1
+   ),
+   
+ ]
 
 animate()
 
