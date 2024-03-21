5ff37294effa2eab8d83afc7cf74027d6217c030
Jon Love <124639155+jonnny013@users.noreply.github.com>
2024-03-21 16:31:03 +0800

    continue to set up visualizer

--- visualizer.js
+++ visualizer.js
@@ -1,4 +1,4 @@
-class Visualizer{
+class Visualizer {
   static drawNetwork(ctx, network) {
     const margin = 50
     const left = margin
@@ -10,21 +10,43 @@ class Visualizer{
   }
 
   static drawLevel(ctx, level, left, top, width, height) {
-    const right = left +width
+    const right = left + width
     const bottom = top + height
 
+    const { inputs, outputs } = level
     const nodeRadius = 18
 
-    for (let i = 0; i < level.inputs.length; i++) {
-      const x = lerp(
-        left,
-        right,
-        level.inputs.length == 1 ? 0.5 : i / (level.inputs.length -1)
-      )
+    for (let i = 0; i < inputs.length; i++) {
+      const x = Visualizer.#getNodeX(inputs, i, left, right)
       ctx.beginPath()
       ctx.arc(x, bottom, nodeRadius, 0, Math.PI * 2)
-      ctx.fillStyle = 'white' 
+      ctx.fillStyle = 'white'
       ctx.fill()
     }
+    for (let i = 0; i < outputs.length; i++) {
+      const x = Visualizer.#getNodeX(outputs, i, left, right)
+      ctx.beginPath()
+      ctx.arc(x, top, nodeRadius, 0, Math.PI * 2)
+      ctx.fillStyle = 'white'
+      ctx.fill()
+    }
+    for (let i = 0; i < inputs.length; i++) {
+      for (let j = 0; j < outputs.length; j++) {
+        ctx.beginPath()
+        ctx.moveTo(Visualizer.#getNodeX(inputs, i, left, right), bottom)
+        ctx.lineTo(Visualizer.#getNodeX(outputs, j, left, right), top)
+        ctx.lineWidth = 2
+        ctx.strokeStyle= 'orange'
+        ctx.stroke()
+      }
+    }
+
+  }
+  static #getNodeX(nodes, index, left, right) {
+    return lerp(
+      left,
+      right,
+      nodes.length == 1 ? 0.5 : index / (nodes.length -1)
+    )
   }
 }
